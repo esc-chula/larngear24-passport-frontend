4d4b5f18571dc8a696cc461c44cf9d687376bd0b
phongit.kha <153442976+Phongit-Khanthawisood@users.noreply.github.com>
2024-11-02 23:16:02 +0700

    feat: update src/env.js
    Co-authored-by: Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>

--- src/env.js
+++ src/env.js
@@ -11,8 +11,8 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string()
         : z.string().optional(),
-    AUTH_DISCORD_ID: z.string(),
-    AUTH_DISCORD_SECRET: z.string(),
+    AUTH_GOOGLE_CLIENT_ID: z.string(),
+    AUTH_GOOGLE_CLIENT_SECRET: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
