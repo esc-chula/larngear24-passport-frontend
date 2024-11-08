f943acc05a3bec1f3419a1692f0fa8d73149b2e2
wnmayy <wanathachat@gmail.com>
2024-11-09 01:34:16 +0700

    feat: no styling memory page

--- src/app/Memory/page.tsx
+++ src/app/Memory/page.tsx
@@ -1,3 +1,35 @@
+
 export default function Memory() {
-  return <h1>Hi this is Memory page</h1>;
+  return (
+    <>
+    <div className="flex h-full flex-col items-center justify-end space-y-[5%]">
+    <div>
+      <h1 className="flex justify-center">Memory</h1>
+      <img src="https://placehold.co/600x400"/>
+    </div>
+    <div>
+    <p className="flex justify-center">อยากฝากอะไรถึง LG24 ?</p>
+
+      <div className="flex justify-end">
+      <button>Add yours</button>
+      </div>
+    </div>
+    <div>
+      <p className="flex justify-center">Photo</p>
+      <p className="flex justify-center">ส่วนบ้าน</p>
+      <div>
+        {/* ธง */}
+      </div>
+    </div>
+    <div>
+      <p className="flex justify-center">ส่วนกลาง</p>
+      <img src="https://placehold.co/300x200"/>
+    </div>
+    <div>
+      <p className="flex justify-center">Video</p>
+    </div>
+
+    </div>
+    </>
+  )
 }
