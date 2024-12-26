aeadbbc6f956e5f14b00ebfba4d8a7851d8588be
neennera <neennera@gmail.com>
2024-12-26 22:57:19 +0700

    style: image placeholder

--- src/app/Memory/page.tsx
+++ src/app/Memory/page.tsx
@@ -22,7 +22,7 @@ export default function Memory() {
           <div className="font-vimamsa text-4xl font-normal">Memory</div>
         </div>
         <div className="item-center mx-4 flex justify-center">
-          <img src="https://placehold.co/600x400" />
+          <img src="/memory/larngear_placeholder.webp" />
         </div>
 
         <div className="item-center mb-3 mt-8 flex justify-center font-ibm text-xl font-bold">
