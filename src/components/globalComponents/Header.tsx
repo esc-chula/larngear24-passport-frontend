c3d098a453c5c5acdfedf7265dbe13479d9407e2
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-12-27 22:37:37 +0700

    feat: add dropdown in header

--- src/components/globalComponents/Header.tsx
+++ src/components/globalComponents/Header.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   const { status } = useSession();
@@ -10,21 +18,38 @@ const Header = () => {
         <Image src="/logo.webp" height={60} width={60} alt="larngear logo" />
         <h1 className="font-vimamsa text-4xl text-[#ECF0F6]">Larngear 24</h1>
         <button
-          onClick={() => {
+          onClick={async () => {
             if (status !== "authenticated") {
-              signIn("google");
+              await signIn("google");
             } else {
-              signOut();
+              await signOut();
             }
           }}
-        >
-          <Image
-            src="/main/fluent_person-20-filled.webp"
-            width={40}
-            height={40}
-            alt="profile icon"
-          />
-        </button>
+        ></button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Image
+              src="/main/fluent_person-20-filled.webp"
+              width={40}
+              height={40}
+              alt="profile icon"
+            />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {status === "authenticated" ? (
+              <DropdownMenuItem>
+                <button onClick={() => signOut()}>Logout</button>
+              </DropdownMenuItem>
+            ) : (
+              <DropdownMenuItem>
+                <button onClick={() => signIn("google")}>Login</button>
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
