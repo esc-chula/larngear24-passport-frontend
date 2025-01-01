a4c4e29390631fdaeb154f5e5afea45ca7ab1986
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-01-02 01:26:46 +0700

    feat: add link to main page on Header

--- src/components/globalComponents/Header.tsx
+++ src/components/globalComponents/Header.tsx
@@ -9,14 +9,20 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 const Header = () => {
   const { status } = useSession();
+
   return (
     <header className="flex h-[104px] w-full flex-row items-end justify-between bg-gradient-to-b from-[#359AB9]/50 to-[#ECF0F7]/50">
       <div className="flex w-full items-center justify-between px-5">
-        <Image src="/logo.webp" height={60} width={60} alt="larngear logo" />
-        <h1 className="font-vimamsa text-4xl text-[#ECF0F6]">Larngear 24</h1>
+        <Link href="/">
+          <Image src="/logo.webp" height={60} width={60} alt="larngear logo" />
+        </Link>
+        <Link href="/">
+          <h1 className="font-vimamsa text-4xl text-[#ECF0F6]">Larngear 24</h1>
+        </Link>
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Image
