4991399c7fe0478454cfb67bdbb4a356a684ee2d
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-01-02 02:43:03 +0700

    fix: redirect to unlock item failed

--- src/components/authProvider.tsx
+++ src/components/authProvider.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 export default function AuthProvider({ children }: React.PropsWithChildren) {
   const { data: session } = useSession();
   const router = useRouter();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const item = searchParams.get("param");
 
   useEffect(() => {
     if (session === null && pathname !== "/Login") {
-      router.push(`/Login?next=${pathname}`);
+      router.push(`/Login?next=${pathname}${item ? `?param=${item}` : ""}`);
     }
   }, [session, router, pathname]);
 
